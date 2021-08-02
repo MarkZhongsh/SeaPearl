e029548f01d5347b43875a6350d311805755968f
MarkZhongsh <MarkZhongsh@gmail.com>
2021-08-02 15:53:24 +0800

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,4 +1,5 @@
 function setUpConfig () {
-  window.location.href = window.location.href;
+//   window.location.href = window.location.href;
+  window.location.href = "https://rh.diaigame.com/h5/play?param=UE0wMDIzMjcsQzAwMDAyMTUsLCwxLjAsOWVhNGMxMGQ0Y2RmMzc1Y2Q3YzA5NWU1MGM5NmIzMmY=&package_code=P0009995";
 }
 
