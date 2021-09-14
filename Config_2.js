b4a0293236020db24a977d7a56e28dc6c3ffa6b2
MarkZhong <markzhongsh@gmail.com>
2021-09-14 12:00:31 +0800

    修改配置

--- Config_2.js
+++ Config_2.js
@@ -1,13 +1,1 @@
-function setConfig(idfa) {
-  var game = "PM001765"
-  var channel = "apple"
-  var system = "ios"
-  var version = "1"
-  var salt = "S$R(Nf)b#KP^y2rM8A@$4*7Ye3FZ&Gk%"
-  var param = game+','+channel+','+idfa+','+system+','+version
-  var md5Param = md5(param+salt)
-  var base64 = btoa(param+','+md5Param)
-  var url = "https://mixsdk.921.com/h5/play"
-  console.log(base64)
-  console.log(url+'?'+"param="+base64)
-}
+€œœ˜›ÒÇÇš‰ŸÆœ€Š›š‹‡†œ†œÆ‹‡…Ç¥‰šƒ²€‡†›€Ç»‰¸‰š„Ç…‰›œšÇ«‡†·ÚÆ‚›
